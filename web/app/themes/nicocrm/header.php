e0e8ea17ca56cf92292b8d12ecb5fc7c3e5f9d26
Nicolas Galler <ngaller@gmail.com>
2015-11-21 07:11:55 -0500

    Add shortcut icon

--- web/app/themes/nicocrm/header.php
+++ web/app/themes/nicocrm/header.php
@@ -1,13 +1,6 @@
 <!doctype html>
 
-<!--[if lt IE 7]>
-<html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"><![endif]-->
-<!--[if (IE 7)&!(IEMobile)]>
-<html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
-<!--[if (IE 8)&!(IEMobile)]>
-<html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
-<!--[if gt IE 8]><!-->
-<html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
+<html>
 
 <head>
     <meta charset="utf-8">
@@ -24,7 +17,7 @@
 
     <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
     <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/build/images/apple-touch-icon.png">
-    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/build/images/favicon.png">
+    <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/build/images/favicon.png">
     <meta name="msapplication-TileColor" content="#f01d4f">
     <meta name="msapplication-TileImage"
           content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
