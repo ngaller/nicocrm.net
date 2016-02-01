3b794054cec56d01a693ed38d0790d1c0c1e5c5c
Nicolas Galler <ngaller@gmail.com>
2016-02-01 15:19:05 -0500

    Add Portfolio page template

---
+++ web/app/themes/nicocrm/page-portfolio.php
@@ -0,0 +1,37 @@
+<?php
+/*
+ Template Name: Portfolio Page
+ */
+
+get_header();
+
+query_posts('post_type=page&post_parent='. get_the_ID());
+
+?>
+<div class="page-content">
+    <main class="inner-content u-clearfix" role="main" itemscope itemprop="mainContentOfPage"
+          itemtype="http://schema.org/Blog">
+        <h1 class="page-title" itemprop="headline"><?php the_title(); ?></h1>
+
+
+<?php
+if (have_posts()) {
+    ?>
+    <div class="posts-list"><?php
+    while (have_posts()) {
+        the_post();
+        get_template_part('post-formats/format-summary');
+    }
+    ?></div><?php
+//    bones_page_navi();
+} else {
+    get_template_part('notfound');
+}
+?>
+
+</main>
+
+<?php // get_sidebar(); ?>
+</div>
+
+<?php get_footer(); ?>
